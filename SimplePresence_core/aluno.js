34a613b6e4849c7847b1e332a8cf125c41407c0a
William Pedro <williamprv@outlook.com>
2023-05-07 21:41:07 -0300

    Add files via upload

---
+++ SimplePresence_core/aluno.js
@@ -0,0 +1,42 @@
+const registrarBtn = document.getElementById('registrar-btn');
+const idInput = document.getElementById('id-input');
+const statusMsg = document.getElementById('status');
+
+const checkStatus = () => {
+  fetch('http://localhost:3000/verificar_chamada')
+    .then(response => response.json())
+    .then(data => {
+      if (data) {
+        registrarBtn.disabled = false;
+        statusMsg.innerHTML = '';
+      } else {
+        registrarBtn.disabled = true;
+        statusMsg.innerHTML = 'Chamada fechada';
+      }
+    })
+    .catch(err => console.error(err));
+}
+
+setInterval(checkStatus, 5000);
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const id = idInput.value;
+
+  fetch('http://localhost:3000/registrar_presenca', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        id: id
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Presença registrada');
+      statusMsg.innerHTML = 'Presença registrada';
+    })
+    .catch(err => console.error(err));
+});
---
+++ simpleProf/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            loop:
+            System.Console.WriteLine("SimplePresence - Test 1.0");
+            System.Console.WriteLine("Digite [1] para iniciar a chamada");
+            System.Console.WriteLine("Digite [2] para fechar a chamada");
+            System.Console.WriteLine("Digite [3] para ver os alunos presentes");
+
+            string baseURL = "http://localhost:3000";
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine("Iniciando chamada...");
+                    HttpWebRequest iniciarChamadaRequest = (HttpWebRequest)WebRequest.Create(baseURL + "/iniciar_chamada");
+                    iniciarChamadaRequest.Method = "POST";
+                    try
+                    {
+                        using (var response = (HttpWebResponse)iniciarChamadaRequest.GetResponse())
+                        {
+                            Console.WriteLine("Chamada iniciada!");
+                        }
+                    }
+                    catch (WebException ex)
+                    {
+                        using (HttpWebResponse response = (HttpWebResponse)ex.Response)
+                        {
+                            using StreamReader streamReader = new StreamReader(response.GetResponseStream());
+                            string result = streamReader.ReadToEnd();
+                            Console.WriteLine(result);
+                        }
+                    }
+                  goto loop;
+                    break;
+
+                case "2":
+                    Console.WriteLine("Fechando chamada...");
+                    HttpWebRequest fecharChamadaRequest = (HttpWebRequest)WebRequest.Create(baseURL + "/fechar_chamada");
+                    fecharChamadaRequest.Method = "POST";
+                    try
+                    {
+                        using (var response = (HttpWebResponse)fecharChamadaRequest.GetResponse())
+                        {
+                            Console.WriteLine("Chamada fechada!");
+                        }
+                    }
+                    catch (WebException ex)
+                    {
+                        using (HttpWebResponse response = (HttpWebResponse)ex.Response)
+                        {
+                            using StreamReader streamReader = new StreamReader(response.GetResponseStream());
+                            string result = streamReader.ReadToEnd();
+                            Console.WriteLine(result);
+                        }
+                    }
+                     goto loop;
+                    break;
+
+                case "3":
+                    Console.WriteLine("Listando alunos presentes...");
+                    HttpWebRequest listarAlunosRequest = (HttpWebRequest)WebRequest.Create(baseURL + "/alunos_presentes");
+                    listarAlunosRequest.Method = "GET";
+                    try
+                    {
+                        using (var response = (HttpWebResponse)listarAlunosRequest.GetResponse())
+                        {
+                            using (var reader = new StreamReader(response.GetResponseStream()))
+                            {
+                                string result = reader.ReadToEnd();
+                                Console.WriteLine(result);
+                            }
+                        }
+                    }
+                    catch (WebException ex)
+                    {
+                        using (HttpWebResponse response = (HttpWebResponse)ex.Response)
+                        {
+                            using StreamReader streamReader = new StreamReader(response.GetResponseStream());
+                            string result = streamReader.ReadToEnd();
+                            Console.WriteLine(result);
+                        }
+                    }
+                     goto loop;
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida.");
+                     goto loop;
+                    break;
+            }
+        }
+    }
+}
