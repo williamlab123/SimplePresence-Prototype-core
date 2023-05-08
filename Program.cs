cf10162ad4949c460131e234e9a28d1a362dd8c1
William Pedro <williamprv@outlook.com>
2023-05-08 14:36:55 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -7,89 +7,97 @@ namespace ConsoleApp
 {
     class Program
     {
-
-        //         TELA DO ALUNO ---  REGISTRAR PRESENÇA ---- 
         static void Main(string[] args)
         {
+            loop:
             System.Console.WriteLine("SimplePresence - Test 1.0");
+            System.Console.WriteLine("Digite [1] para iniciar a chamada");
+            System.Console.WriteLine("Digite [2] para fechar a chamada");
+            System.Console.WriteLine("Digite [3] para ver os alunos presentes");
 
             string baseURL = "https://simplepresence-production.up.railway.app";
-            while (true)
+            string input = Console.ReadLine();
+
+            switch (input)
             {
-                var request = (HttpWebRequest)WebRequest.Create(baseURL + "/verificar_chamada");
-                using (var response = (HttpWebResponse)request.GetResponse())
-                {
-                    using (var reader = new StreamReader(response.GetResponseStream()))
+                case "1":
+                    Console.WriteLine("Iniciando chamada...");
+                    HttpWebRequest iniciarChamadaRequest = (HttpWebRequest)WebRequest.Create(baseURL + "/iniciar_chamada");
+                    iniciarChamadaRequest.Method = "POST";
+                    try
                     {
-                        string result = reader.ReadToEnd();
-                        bool isOpen = bool.Parse(result);
-                        Console.WriteLine(isOpen);
-
-                        if (isOpen)
+                        using (var response = (HttpWebResponse)iniciarChamadaRequest.GetResponse())
                         {
-                            System.Console.WriteLine("A chamada está aberta!");
-                            Console.WriteLine("Digite o ID do aluno:");
-                            string id = Console.ReadLine();
-
-                             request = (HttpWebRequest)WebRequest.Create(baseURL + "/registrar_presenca");
-                            request.Method = "POST";
-
-                            string postData = "id=" + id;
-                            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
-
-                            request.ContentType = "application/x-www-form-urlencoded";
-                            request.ContentLength = byteArray.Length;
-
-                            using (var dataStream = request.GetRequestStream())
-                            {
-                                dataStream.Write(byteArray, 0, byteArray.Length);
-                            }
-
-                            try
-                            {
-                                using (var responsse = (HttpWebResponse)request.GetResponse())
-                                {
-                                    Console.WriteLine("Presença registrada");
-                                }
-                            }
-                            catch (WebException ex)
-                            {
-                                using (HttpWebResponse responsee = (HttpWebResponse)ex.Response)
-                                {
-                                    using StreamReader streamReader = new StreamReader(response.GetResponseStream());
-                                    string resulti = streamReader.ReadToEnd();
-                                    Console.WriteLine(resulti);
-                                }
-                            }
-
-
-
-
-                        }
-                        else
-                        {
-                            System.Console.WriteLine("A chamada está fechada!");
+                            Console.WriteLine("Chamada iniciada!");
                         }
                     }
-                }
-                Thread.Sleep(5000); 
+                    catch (WebException ex)
+                    {
+                        using (HttpWebResponse response = (HttpWebResponse)ex.Response)
+                        {
+                            using StreamReader streamReader = new StreamReader(response.GetResponseStream());
+                            string result = streamReader.ReadToEnd();
+                            Console.WriteLine(result);
+                        }
+                    }
+                  goto loop;
+                    break;
+
+                case "2":
+                    Console.WriteLine("Fechando chamada...");
+                    HttpWebRequest fecharChamadaRequest = (HttpWebRequest)WebRequest.Create(baseURL + "/fechar_chamada");
+                    fecharChamadaRequest.Method = "POST";
+                    try
+                    {
+                        using (var response = (HttpWebResponse)fecharChamadaRequest.GetResponse())
+                        {
+                            Console.WriteLine("Chamada fechada!");
+                        }
+                    }
+                    catch (WebException ex)
+                    {
+                        using (HttpWebResponse response = (HttpWebResponse)ex.Response)
+                        {
+                            using StreamReader streamReader = new StreamReader(response.GetResponseStream());
+                            string result = streamReader.ReadToEnd();
+                            Console.WriteLine(result);
+                        }
+                    }
+                     goto loop;
+                    break;
+
+                case "3":
+                    Console.WriteLine("Listando alunos presentes...");
+                    HttpWebRequest listarAlunosRequest = (HttpWebRequest)WebRequest.Create(baseURL + "/alunos_presentes");
+                    listarAlunosRequest.Method = "GET";
+                    try
+                    {
+                        using (var response = (HttpWebResponse)listarAlunosRequest.GetResponse())
+                        {
+                            using (var reader = new StreamReader(response.GetResponseStream()))
+                            {
+                                string result = reader.ReadToEnd();
+                                Console.WriteLine(result);
+                            }
+                        }
+                    }
+                    catch (WebException ex)
+                    {
+                        using (HttpWebResponse response = (HttpWebResponse)ex.Response)
+                        {
+                            using StreamReader streamReader = new StreamReader(response.GetResponseStream());
+                            string result = streamReader.ReadToEnd();
+                            Console.WriteLine(result);
+                        }
+                    }
+                     goto loop;
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida.");
+                     goto loop;
+                    break;
             }
-
-
-
-
-
-
-
-
-
-
-
-
-
         }
-
-        
     }
 }
-
