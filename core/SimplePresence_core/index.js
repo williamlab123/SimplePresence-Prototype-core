cd036413563a8b96f9c25efd845595a533226c6f
William Pedro <williamprv@outlook.com>
2023-05-12 00:18:35 -0300

    Add files via upload

--- core/SimplePresence_core/index.js
+++ core/SimplePresence_core/index.js
@@ -4,40 +4,65 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const wifi = require('node-wifi');
+const mysql = require('mysql2');
+const path = require('path');
+const readline = require('readline');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const path = require('path');
 
-const database = {
-  "2076937": {
-    "name": "Alice",
-    "course": "Computer Science",
-    "class": "3A"
-  },
-  "1234567": {
-    "name": "Bob",
-    "course": "Physics",
-    "class": "1B"
-  },
-  "9876543": {
-    "name": "Charlie",
-    "course": "Chemistry",
-    "class": "2C"
-  },
-  "5555555": {
-    "name": "David",
-    "course": "Biology",
-    "class": "4D"
-  },
-  "1111111": {
-    "name": "Eve",
-    "course": "Mathematics",
-    "class": "5E"
-  }
-};
+
+const connection = mysql.createConnection({
+  host: '192.168.1.106', // Substitua pelo endereço IP do outro computador
+  user: 'root',
+  password: '12345',
+  database: 'alunos'
+});
+
+
+function verificarAluno(id) {
+  return new Promise((resolve, reject) => {
+    const query = `SELECT * FROM alunos WHERE id = ${id}`;
+    connection.query(query, (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results.length > 0); // Retorna true se houver resultados, caso contrário, retorna false
+      }
+    });
+  });
+}
+
+
+// const database = {
+//   "2076937": {
+//     "name": "Alice",
+//     "course": "Computer Science",
+//     "class": "3A"
+//   },
+//   "1234567": {
+//     "name": "Bob",
+//     "course": "Physics",
+//     "class": "1B"
+//   },
+//   "9876543": {
+//     "name": "Charlie",
+//     "course": "Chemistry",
+//     "class": "2C"
+//   },
+//   "5555555": {
+//     "name": "David",
+//     "course": "Biology",
+//     "class": "4D"
+//   },
+//   "1111111": {
+//     "name": "Eve",
+//     "course": "Mathematics",
+//     "class": "5E"
+//   }
+// };
 
 let alunos_presentes = [];
 var ssidAluno = "";
@@ -62,11 +87,15 @@ function getCurrentSSID() {
   });
 }
 
+
+
 getCurrentSSID().then((ssid) => {
-  console.log('SSID atual:', ssid);
+  ssidProf = ssid;
+  console.log('SSID atual do professor:', ssidProf);
 }).catch((err) => {
   console.log(err);
 });
+
 
 let chamadaAberta = false;
 
@@ -81,15 +110,38 @@ app.get('/', function (req, res) {
 app.get('/aluno', function (req, res) {
   res.sendFile(path.join(__dirname, 'aluno.html'));
 
-  
-  getCurrentSSID().then((ssid) => {
-    console.log('SSID atual:', ssid);
-  }).catch((err) => {
+  getCurrentSSID()
+  .then((ssid) => {
+    ssidAluno = ssid; // Atribui o valor do ssid à variável ssidAluno
+    console.log('ssid do aluno:', ssidAluno);
+  })
+  .catch((err) => {
     console.log(err);
   });
 
-    ssidAluno = ssid;
   
+//   ssidAluno =  getCurrentSSID().then((ssid) => {
+
+//   console.log('ssid do aluno:', ssidAluno);
+// }).catch((err) => {
+//   console.log(err);
+// });
+
+  
+
+  
+ 
+
+
+  // ssid = getCurrentSSID().then((ssid) => {
+  //   console.log('SSID atual:', ssid);
+  // }).catch((err) => {
+  //   console.log(err);
+  // });
+
+ // ssidAluno = ssid;
+
+
 });
 
 
@@ -141,6 +193,11 @@ app.get('/verificar_chamada', function (req, res) {
 });
 
 
+app.get('/ssid', function (req, res) {
+  res.send(ssidAluno)
+})
+
+
 app.get('/alunos_presentes', function (req, res) {
   res.send(alunos_presentes)
 })
@@ -150,24 +207,45 @@ app.post('/registrar_presenca', function (req, res) {
   console.log('Chamada aberta:', chamadaAberta);
   console.log('ID inserido:', id);
   if (chamadaAberta && ssidAluno === ssidProf) {
-    if (database.hasOwnProperty(id)) {
 
-      console.log(`Aluno registrado: ${id} - Nome: ${database[id].name} - Curso: ${database[id].course} - Turma: ${database[id].class}`);
-      alunos_presentes.push(id);
+    verificarAluno(id)
+      .then(existe => {
+        if (existe) {
+          console.log(`O aluno com ID ${id} registrou presença.`);
+          res.send("Sua presença foi registrada!")
+          alunos_presentes.push(id)
+        } else {
+          console.log(`O aluno com ID ${id} não foi encontrado no banco de dados.`);
+          res.send("Id insediro incorreto!")
+        }
+      })
+      .catch(err => {
+        console.error('Erro ao verificar aluno:', err);
+      });
+    // if (database.hasOwnProperty(id)) {
+
+    //   console.log(`Aluno registrado: ${id} - Nome: ${database[id].name} - Curso: ${database[id].course} - Turma: ${database[id].class}`);
+    //   alunos_presentes.push(id);
 
 
 
-      res.json({ status: 'OK' });
-    } else {
-      console.log("id nao encontrado")
-      res.status(400).json({ error: 'ID não encontrado' });
-    }
-  } else if (ssidAluno !== ssidProf) {
+    //   res.json({ status: 'OK' });
+    // } else {
+    //   console.log("id nao encontrado")
+    //   res.status(400).json({ error: 'ID não encontrado' });
+    // }
+
+
+
+
+  } else if (ssidAluno !== ssidProf)
+   {
     console.log("Voce precisa estar na mesma rede para registrar presença!")
     res.status(400).json({ error: "Voce precisa estar na mesma rede para registrar presença!" })
   }
 
-  else {
+  else
+   {
     console.log("chamada fechada")
     res.status(400).json({ error: 'Chamada fechada' });
   }
@@ -184,7 +262,7 @@ const server = http.listen(3000, function () {
 });
 
 // Código para adicionar
-const readline = require('readline');
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
